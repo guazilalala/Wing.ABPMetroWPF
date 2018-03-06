af28d8df435dea720d69978c2955226b176c7ea5
Feet <59450804@qq.com>
2018-03-07 01:01:42 +0800

    home20180307

--- BingShengReportToBill/BingShengReportToBill/ViewModel/MainViewModel.cs
+++ BingShengReportToBill/BingShengReportToBill/ViewModel/MainViewModel.cs
@@ -3,6 +3,7 @@ using BingShengReportToBill.Model;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Threading;
+using MahApps.Metro.Controls.Dialogs;
 using NLog;
 using System;
 using System.Collections;
@@ -12,6 +13,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using MahApps.Metro.Controls;
 
 namespace BingShengReportToBill.ViewModel
 {
@@ -33,17 +35,95 @@ namespace BingShengReportToBill.ViewModel
 		private readonly AppConfig _appConfig = new AppConfig();
 		private ObservableCollection<Order> _orders;
 		private Order _selectedOrder;
-		private int _selectedIndex;
 		/// <summary>
 		/// Initializes a new instance of the MainViewModel class.
 		/// </summary>
 		public MainViewModel()
 		{
 			UploadCommand = new RelayCommand<DateTime>(x => UploadOrder(x));
+
+			UploadButtonEnabled = true;
+			UploadTipsVisibility = false;
 		}
 
 
 		#region Properties
+		private int _successfulCount { get; set; }
+		public int SuccessfulCount
+		{
+			get
+			{
+				return _successfulCount;
+			}
+
+			set
+			{
+				_successfulCount = value;
+				RaisePropertyChanged(() => SuccessfulCount);
+			}
+		}
+
+		private int _failuresCount { get; set; }
+		public int FailuresCount
+		{
+			get
+			{
+				return _failuresCount;
+			}
+
+			set
+			{
+				_failuresCount = value;
+				RaisePropertyChanged(() => FailuresCount);
+			}
+		}
+
+		private bool _processingEnabled;
+		public bool ProcessingEnabled
+		{
+			get
+			{
+				return _processingEnabled;
+			}
+
+			set
+			{
+				_processingEnabled = value;
+				RaisePropertyChanged(() => ProcessingEnabled);
+			}
+		}
+
+		private bool _uploadTipsVisibility;
+		public bool UploadTipsVisibility
+		{
+			get
+			{
+				return _uploadTipsVisibility;
+			}
+
+			set
+			{
+				_uploadTipsVisibility = value;
+				RaisePropertyChanged(() => UploadTipsVisibility);
+			}
+		}
+
+		private bool _uploadButtonEnabled;
+		public bool UploadButtonEnabled
+		{
+			get
+			{
+				return _uploadButtonEnabled;
+			}
+
+			set
+			{
+				_uploadButtonEnabled = value;
+				ProcessingEnabled = !_uploadButtonEnabled;
+				RaisePropertyChanged(() => UploadButtonEnabled);
+			}
+		}
+
 		public ObservableCollection<Order> Orders
 		{
 			get
@@ -69,17 +149,6 @@ namespace BingShengReportToBill.ViewModel
 			}
 		}
 
-
-		public int SelectedIndex
-		{
-			get { return _selectedIndex; }
-			set
-			{
-				_selectedIndex = value;
-				RaisePropertyChanged(() => SelectedIndex);
-			}
-		}
-
 		#endregion
 
 		#region Functions
@@ -94,24 +163,27 @@ namespace BingShengReportToBill.ViewModel
 			}
 			#endregion
 
-
+			UploadButtonEnabled = false;
+			UploadTipsVisibility = true;
+			SuccessfulCount = 0;
+			FailuresCount = 0;
 
 			var uploadDate = date.ToString("yyyy-MM-dd");
 
 			Task.Factory.StartNew(() =>
 			{
-				while (true)
+				for (int i = 0; i < 100; i++)
 				{
 					DispatcherHelper.CheckBeginInvokeOnUI(() =>
 					{
 						var order = new Order { Id = DateTime.Now.ToString("yyyyMMddHHmmss"), UploadSuccess = true, UploadTime = DateTime.Now };
 						Orders.Add(order);
+						SuccessfulCount++;
 						SelectedOrder = order;
 					});
-
-					Thread.Sleep(1000);
+					Thread.Sleep(100);
 				}
-
+				UploadButtonEnabled = true;
 			});
 		}
 
